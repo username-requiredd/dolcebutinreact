91b7e9bf7d121ced4161eec8b329180461b91e75
username-requiredd <pj337167@gmail.com>
2024-05-26 14:44:17 -1200

    "fixed api problems "

--- src/pages/productslist/Products.jsx
+++ src/pages/productslist/Products.jsx
@@ -10,14 +10,37 @@ import renderSkeleton from "../../components/loaders/skeleton";
 import debounce from "lodash.debounce";
 const Products = () => {
   const [state, setState] = useState("");
-  const [page, setPage] = useState(70);
+  const [page, setPage] = useState(0);
   const [searchQuery, setSearchQuery] = useState("");
-  const url = `https://dummyjson.com/products?limit=12&skip=${page}`;
+  const url = `https://dummyjson.com/products?limit=0&skip=${page}`;
   const searchUrl = `https://dummyjson.com/products/search?q=${state}`;
 
   const { data, error, loading } = useFetch(searchQuery ? searchUrl : url);
+  const categories = [
+    "beauty",
+    "tops",
+    "mens-shoes",
+    "skin-care",
+    "mens-watches",
+    "sunglasses",
+    "womens-bags",
+    "womens-dresses",
+    "womens-jewellery",
+    "womens-shoes",
+    "womens-bags",
+    "womens-watches",
+    "fragrances",
+  ];
+
+  const filteredData =
+    data && data.products
+      ? data.products.filter(({ category }) => categories.includes(category))
+      : [];
+
+  console.log("categories:", filteredData);
   const search = useFetch(searchUrl);
-  console.log("search", search);
+  // console.log("search", search);
+  console.log(data);
   const renderSkeletonColumns = () => {
     let columns = [];
     for (let i = 0; i < 8; i++) {
@@ -51,14 +74,14 @@ const Products = () => {
             <div className="grid-container">{renderSkeletonColumns()}</div>
           ) : error ? (
             <h6>Error getting data!</h6>
-          ) : data.products && data.products.length === 0 ? (
+          ) : data.products && filteredData.length === 0 ? (
             <h6 className="mx-auto ">
               {" "}
               That products got lost in the Bermuda Triangle.
             </h6>
-          ) : data && data.products ? (
+          ) : data && filteredData ? (
             <div className="grid-container">
-              {data.products.map(({ images, price, title, id }) => (
+              {filteredData.map(({ images, price, title, id }) => (
                 <ProductCard
                   images={images}
                   price={price}
@@ -77,9 +100,7 @@ const Products = () => {
             searchQuery ? "d-none" : "d-flex justify-content-center"
           } align-items-center`}
         >
-          <div>
-            <ButtonGroup setPage={setPage} />
-          </div>
+          <div>{/* <ButtonGroup setPage={setPage} /> */}</div>
         </div>
       </div>
     </>
